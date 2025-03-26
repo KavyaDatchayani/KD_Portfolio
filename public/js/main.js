2562b6b1bbc1a1fbb386d64bcad0bf1b470a78ed
Kavya Datchayani <kavyadatchayani@gmail.com>
2025-03-26 21:15:46 +0530

    Initial commit without about page

---
+++ public/js/main.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const nav = document.getElementById("navbar");
+    const menuBar = document.getElementById("menubar");
+    const navLinks = document.querySelector(".navlinks");
+    const navlinka = document.querySelectorAll(".navlinks a");
+
+    const line1 = document.getElementById("lineone");
+    const line2 = document.getElementById("linetwo");
+    const line3 = document.getElementById("linethree");
+
+    menuBar.addEventListener("click", function () {
+        navLinks.classList.toggle("show");
+
+        line1.classList.toggle("slidedown");
+        line1.classList.toggle("change");
+        line2.classList.toggle("slideup");
+        line2.classList.toggle("change");
+        line3.classList.toggle("disappear");
+    });
+
+    navlinka.forEach(links => {
+        links.addEventListener("click", function () {
+            navLinks.classList.remove("show");
+            line1.classList.toggle("slidedown");
+            line1.classList.toggle("change");
+            line2.classList.toggle("slideup");
+            line2.classList.toggle("change");
+            line3.classList.toggle("disappear");
+        });
+    });
+
+    var oldScroll = window.scrollY;
+
+    window.addEventListener("scroll", function () {
+        var cscroll = window.scrollY;
+        if (oldScroll > cscroll) {
+            nav.style.top = "0";
+        } else {
+            nav.style.top = "-100%";
+        }
+        oldScroll = cscroll;
+    });
+
+
+    document.getElementById("contactForm").addEventListener("submit", async function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message").value;
+
+        try {
+            const response = await fetch("/submit", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, email, message })
+            });
+
+            const result = await response.json();
+
+
+            document.getElementById("popupMessage").innerHTML = `${result.message} <i class="fa-regular fa-circle-check"></i>`;
+
+
+            document.getElementById("popup").classList.add("show");
+
+
+            document.getElementById("contactForm").reset();
+
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    });
+
+
+    document.getElementById("closeBtn").addEventListener("click", function () {
+        document.getElementById("popup").classList.remove("show");
+    });
+});
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express()
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+
+
+const mongoURI = process.env.MONGO_URI;
+const dbName = process.env.DBNAME;
+let db;
+
+// Connecting to MongoDB
+
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+const client = new MongoClient(mongoURI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Pinged your deployment. Successfully connected to MongoDB!");
+        db = client.db(dbName);
+    } catch (error) {
+        console.error("MongoDB Connection Error:", error);
+    }
+}
+run();
+
+// routing api
+app.post("/submit", async (req, res) => {
+    const { username, email, message } = req.body;
+    if (!db) return res.status(500).json({ success: false, error: "Database not connected" });
+
+    try {
+        const collection = db.collection("messages");
+        await collection.insertOne({ username, email, message });
+        res.status(200).json({ success: true, message: "Message received successfully!" });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(port, () => {
+    console.log(`Running without any Problem  ${port}`)
+})
